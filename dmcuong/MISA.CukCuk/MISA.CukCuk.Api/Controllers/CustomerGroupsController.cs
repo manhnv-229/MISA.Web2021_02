dfe933697209bf90330450af1b640bea182ccbb0
mcd_oblivion <mcd.oblivion@gmail.com>
2021-02-08 23:37:04 +0700

    Base class and interface

---
+++ dmcuong/MISA.CukCuk/MISA.CukCuk.Api/Controllers/CustomerGroupsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MISA.Common.Models;
+using MISA.Service;
+using MISA.Service.Interfaces;
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    [Route("api/v1/customer-groups")]
+    public class CustomerGroupsController : BaseController<CustomerGroup>
+    {
+        #region Constructor
+
+        public CustomerGroupsController(IBaseService<CustomerGroup> baseService) : base(baseService)
+        {
+        }
+
+        #endregion Constructor
+    }
+}
