eabf724fd2d56c103c99269ef45307d39040c14c
NgocSon15 <ngocson1562k@gmail.com>
2021-02-08 22:31:05 +0700

    them project api

---
+++ mf722-nnson1/MISA.BTL.API/MISA.BTL.Business/Interfaces/ICustomerGroupBusiness.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MISA.BTL.Common;
+
+namespace MISA.BTL.Business.Interfaces
+{
+    public interface ICustomerGroupBusiness:IBaseBusiness<CustomerGroup>
+    {
+    }
+}
