c3ef70a1e6115f2dd3281798809d7bd388b28dc0
Nguyen Thi Thiem <Nguyenthithiem2000@gmail.com>
2021-02-08 23:45:48 +0700

    MF731-NTTHIEM BT XAY DUNG KIEN TRUC UNG DUNG

---
+++ mf731-ntthiem/MISA.CukCuk/MISA.CukCuk.Api/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MISA.Service;
+using MISA.Common.Models;
+using MISA.Service.Interfaces;
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    [Route("api/v1/customers")]
+    public class CustomersController : BaseController<Customer>
+    {
+        /// <summary>
+        /// Khởi tạo biến kết nối với CustomerService và truyền thông tin lên base
+        /// </summary>
+        /// <param name="customerService">mBiến để truyền lên base </param>
+        /// CreatedBy: NTTHIEM
+        /// CreatedDate: 08/02/2021
+        public CustomersController(ICustomerService _customerService) :base(_customerService)
+        {
+
+        }
+    }
+}
---
+++ mf731-ntthiem/MISA.CukCuk/MISA.DataLayer/CustomerRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Linq;
+using MISA.Common.Models;
+namespace MISA.DataLayer
+{
+    public class CustomerRepository: DbContext<Customer>
+    {
+        /// <summary>
+        /// Lấy 10 KH đầu tiên
+        /// </summary>
+        /// <returns></returns>
+        /// CreatedBy: NTTHIEM
+        /// CreatedDate: 07/02/2021
+        public IEnumerable<Customer> GetCustomerTop10()
+        {
+            return GetData("SELECT * FROM Customer LIMIT 10");
+        }
+
+        /// <summary>
+        /// Kiểm tra mã khách hàng đã tồn tại chưa
+        /// </summary>
+        /// <param name="customerCode">Mã khách hàng cần kiểm tra</param>
+        /// <returns>true: đã tồn tại; false: chưa tồn tại</returns>
+        /// CreatedBy: NTTHIEM
+        /// CreatedDate: 07/02/2021
+        public bool CheckCustomerCodeExist(string customerCode)
+        {
+            var sql = $"SELECT CustomerCode FROM Customer WHERE CustomerCode = '{customerCode}'";
+            var customerCodeExist = _dbConnection.Query<string>(sql).FirstOrDefault();
+            if (customerCodeExist != null)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Kiểm tra số điện thoại đã tồn tại chưa
+        /// </summary>
+        /// <param name="phoneNumber">SĐT cần kiểm tra</param>
+        /// <returns>true - đã tồn tại hoặc false - chưa tồn tại</returns>
+        /// CreatedBy: NTTHIEM
+        /// CreatedDate: 07/02/2021
+        public bool CheckPhoneNumberExist(string phoneNumber)
+        {
+            var sql = $"SELECT PhoneNumber FROM Customer WHERE PhoneNumber = '{phoneNumber}'";
+            var phoneNumberExist = _dbConnection.Query<string>(sql).FirstOrDefault();
+            if (phoneNumberExist != null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
