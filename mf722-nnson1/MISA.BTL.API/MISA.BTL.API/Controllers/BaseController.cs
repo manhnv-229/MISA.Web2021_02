59bf8a65f35a0778ab4f3ea7254e0fab1faff218
NgocSon15 <ngocson1562k@gmail.com>
2021-02-08 20:53:36 +0700

    nop bai API

---
+++ mf722-nnson1/MISA.BTL.API/MISA.BTL.API/Controllers/BaseController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.BTL.Business;
+using MISA.BTL.Business.Interfaces;
+
+namespace MISA.BTL.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseController<T> : ControllerBase
+    {
+        IBaseBusiness<T> _baseBusiness;
+        public BaseController(IBaseBusiness<T> baseBusiness)
+        {
+            _baseBusiness = baseBusiness;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var res = _baseBusiness.GetData();
+            var customers = res.Data as List<T>;
+            if (customers.Count == 0)
+            {
+                return StatusCode(204, res.Data);
+            }
+            else
+            {
+                return StatusCode(200, res.Data);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] T entity)
+        {
+            var res = _baseBusiness.Insert(entity);
+            switch (res.Success)
+            {
+                case false:
+                    return StatusCode(400, res.Data);
+                case true:
+                    return StatusCode(201, res.Data);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] T entity)
+        {
+            var res = _baseBusiness.Update(entity);
+            switch (res.Success)
+            {
+                case false:
+                    return StatusCode(400, res.Data);
+                case true:
+                    return StatusCode(201, res.Data);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var res = _baseBusiness.Delete(id);
+            switch (res.Success)
+            {
+                case false:
+                    return StatusCode(400, res.Data);
+                case true:
+                    return StatusCode(200, res.Data);
+            }
+        }
+    }
+}
---
+++ mf722-nnson1/MISA.BTL.API/MISA.BTL.Business/Business/CustomerGroupBusiness.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MISA.BTL.Business.Interfaces;
+using MISA.BTL.Common;
+using MISA.BTL.Database;
+using MISA.BTL.Database.Interfaces;
+
+namespace MISA.BTL.Business
+{
+    public class CustomerGroupBusiness : BaseBusiness<CustomerGroup>, ICustomerGroupBusiness
+    {
+        IDbConnector<CustomerGroup> _dbConnector;
+        public CustomerGroupBusiness(IDbConnector<CustomerGroup> dbConnector):base(dbConnector)
+        {
+            _dbConnector = dbConnector;
+        }
+
+        /*public ServiceResult GetDataById(string id)
+        {
+            var serviceResult = new ServiceResult();
+            var errorMsg = new ErrorMsg();
+            if (customerGroupRepository.CheckCustomerGroupIdExist(id) == false)
+            {
+                errorMsg.UserMsg.Add(Properties.Resources.Erro_Group_Not_Exist);
+                serviceResult.Data = errorMsg;
+                serviceResult.Success = false;
+            }
+            else
+            {
+                serviceResult.Data = customerGroupRepository.GetData($"Proc_GetCustomerGroupById", new { CustomerGroupId = id }, System.Data.CommandType.StoredProcedure);
+                serviceResult.Success = true;
+            }
+            return serviceResult;
+        }*/
+
+        protected override bool ValidateData(CustomerGroup customerGroup, ErrorMsg errorMsg = null)
+        {
+            var isValid = true;
+            // 1. check trùng mã: 
+            if (_dbConnector.CheckEmptyCustomerGroupName(customerGroup.CustomerGroupName) == true)
+            {
+                errorMsg.UserMsg.Add(Properties.Resources.Erro_Required_CustomerGroupName);
+                isValid = false;
+            }
+            if (_dbConnector.CheckCustomerGroupNameExist(customerGroup.CustomerGroupName) == true)
+            {
+                errorMsg.UserMsg.Add(Properties.Resources.Erro_Duplicate_CustomerGroupName);
+                isValid = false;
+            }
+
+            return isValid;
+
+        }
+    }
+}
