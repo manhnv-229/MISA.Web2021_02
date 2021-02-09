754665417812703aea1a1ef2d8b2c28ae0b809ad
Mr.Vincent <leephong1807@gmail.com>
2021-02-09 14:15:08 +0700

    Nộp bài tập Kiến trúc ứng dụng

---
+++ mf699-lhphong/MISA.CukCuk.Api/MISA.DATALayer/Interface/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.DATALayer.Interface
+{
+    public interface IDbContext<MisaEntity>
+    {
+        public IEnumerable<MisaEntity> GetAll();
+        public IEnumerable<MisaEntity> GetAll(string sqlCommand, CommandType commandType = CommandType.Text);
+        public IEnumerable<MisaEntity> GetData(string sqlCommand, object parameters = null, CommandType commandType = CommandType.Text);
+        public int InsertObject(MisaEntity entity);
+    }
+}
