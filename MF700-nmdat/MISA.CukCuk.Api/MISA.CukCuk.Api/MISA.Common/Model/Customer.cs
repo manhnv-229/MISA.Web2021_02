ebae3b67014f4b6f3ea4a26fa69b083c94098c0c
minhdatcvp <minhdatptt@gmail.com>
2021-02-09 17:37:36 +0700

    upload file

---
+++ MF700-nmdat/MISA.CukCuk.Api/MISA.CukCuk.Api/MISA.Common/Model/Customer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.Common.Model
+{
+    /// <summary>
+    /// Class Khách hàng
+    /// </summary>
+    /// CreatedBy : NMDat (03/02/2021)
+    public class Customer
+    {
+        #region Contructor
+
+        #endregion
+
+        #region Method
+
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+        /// <summary>
+        /// Họ và tên
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Mã số thẻ
+        /// </summary>
+        public string MemberCardCode { get; set; }
+        /// <summary>
+        /// Khóa ngoại
+        /// </summary>
+        public Guid? CustomerGroupId { get; set; }
+        /// <summary>
+        /// Ngày tháng năm sinh
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+        /// <summary>
+        /// giới tính
+        /// </summary>
+        public int? Gender { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Tên Công ty
+        /// </summary>
+        public string CompanyName { get; set; }
+        /// <summary>
+        /// Mã số thuế
+        /// </summary>
+        public string CompanyTaxCode { get; set; }
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+        #endregion
+
+        #region Other
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// Người tạo
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// Ngày sửa
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+        /// <summary>
+        /// Người sửa
+        /// </summary>
+        public string ModifiedBy { get; set; }
+        #endregion
+    }
+}
---
+++ MF700-nmdat/MISA.CukCuk.Api/MISA.CukCuk.Api/MISA.CukCuk.Api/Controllers/BasesController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Service;
+using MISA.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    /// <summary>
+    /// Base Class cho Controller các controller kế thừa có thể gọi lại các method từ base
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasesController<T> : ControllerBase
+    {
+        /// <summary>
+        /// Khai báo interface tham chiếu đến BaseService
+        /// </summary>
+        IBaseService<T> _baseService;
+        public BasesController(IBaseService<T> baseService)
+        {
+            _baseService = baseService;
+        }
+        /// <summary>
+        /// Lấy toàn bộ dữ liệu đối tượng
+        /// </summary>
+        /// <returns>Danh sách dữ liệu dối tượng</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var res = _baseService.Get();
+            var result = res.Data as List<T>;
+            // Nếu có kết quả trả về result.Count > 0 thì trả về 200 nếu không trả về 204
+            if (result.Count == 0)
+                return StatusCode(204, res.Data);
+            else
+                return StatusCode(200, res.Data);
+        }
+        /// <summary>
+        /// Lấy dữ liệu theo id
+        /// </summary>
+        /// <param name="id">id thực thể</param>
+        /// <returns>Dữ liệu cần lấy theo id</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetById( Guid id)
+        {
+            var res = _baseService.GetById(id);
+            var result = res.Data as List<T>;
+            // Nếu có kết quả trả về result.Count > 0 thì trả về 200 nếu không trả về 204
+            if (result.Count == 0)
+                return StatusCode(204, res.Data);
+            else
+                return StatusCode(200, res.Data);
+        }
+        [HttpGet("Pagination")]
+        public IActionResult GetPagination([FromQuery] int limit,int offset)
+        {
+            var res = _baseService.GetPagination(limit,offset);
+            var result = res.Data as List<T>;
+            // Nếu có kết quả trả về result.Count > 0 thì trả về 200 nếu không trả về 204
+            if (result.Count == 0)
+                return StatusCode(204, res.Data);
+            else
+                return StatusCode(200, res.Data);
+        }
+        // POST api/<CustomersController>
+        /// <summary>
+        /// Thêm đối tượng
+        /// </summary>
+        /// <param name="entity">Thực thể</param>
+        /// <returns>Responsive tương ứng cho Client (201- thành công, ...)</returns>
+        /// CreatedBy: NMDAT (03/02/2021)
+        [HttpPost]
+        public IActionResult Post(T entity)
+        {
+            // Gọi đến hàm Insert thực hiện validate -> thêm
+            var res = _baseService.Insert(entity);
+
+            if (res.Success == false)
+            {
+                return StatusCode(400, res.Data);
+            }
+            else if (res.Success == true && (int)res.Data > 0)
+            {
+                return StatusCode(201, res.Data);
+            }
+            else
+            {
+                return StatusCode(200, res.Data);
+            }
+        }
+
+
+        // POST api/<CustomersController>
+        /// <summary>
+        /// Sửa đối tượng
+        /// </summary>
+        /// <param name="entity">Thực thể</param>
+        /// <returns>Responsive tương ứng cho Client (201- thành công, ...)</returns>
+        /// CreatedBy: NMDAT (03/02/2021)
+        [HttpPut]
+        public IActionResult Put(T entity)
+        {
+            // Gọi đến hàm Insert thực hiện validate -> Sửa
+            var res = _baseService.Update(entity);
+
+            if (res.Success == false)
+            {
+                return StatusCode(400, res.Data);
+            }
+            else if (res.Success == true && (int)res.Data > 0)
+            {
+                return StatusCode(201, res.Data);
+            }
+            else
+            {
+                return StatusCode(200, res.Data);
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete]
+        public IActionResult Delete(T entity)
+        {
+            // Gọi đến hàm Insert thực hiện validate -> Sửa
+            var res = _baseService.Delete(entity);
+
+            if (res.Success == false)
+            {
+                return StatusCode(400, res.Data);
+            }
+            else if (res.Success == true && (int)res.Data > 0)
+            {
+                return StatusCode(201, res.Data);
+            }
+            else
+            {
+                return StatusCode(200, res.Data);
+            }
+        }
+
+    }
+}
